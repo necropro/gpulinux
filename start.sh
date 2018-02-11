050240fb223f36229c297456f5986a9a2e6c9021
necropro <36379735+necropro@users.noreply.github.com>
2018-02-11 20:36:19 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner -eu  europe.equihash-hub.miningpoolhub.com:12023 -NecroPro.miner1 t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --qwsaqwsa z --port 12023
+./miner europe.equihash-hub.miningpoolhub.com:12023 -NecroPro.miner1 -qwsaqwsa z --port 12023
