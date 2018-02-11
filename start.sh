77729c64d534f2f1cdfd5a3aa8565fb2526b60c2
necropro <36379735+necropro@users.noreply.github.com>
2018-02-11 20:23:46 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner -eu  europe.equihash-hub.miningpoolhub.com -NecroPro t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --qwsaqwsa z --port 12023
